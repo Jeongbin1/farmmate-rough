0ed7ef8b9f0a8dce413a6cdd9385e2f9e81419c3
bamgee <petitjb05@gamil.com>
2023-05-13 22:51:12 +0900

    ADD layout-roughversion

---
+++ app/src/main/java/com/example/farmmate1/TestResultActivity.kt
@@ -0,0 +1,11 @@
+package com.example.farmmate1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TestResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test_result)
+    }
+}
