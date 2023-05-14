ea40c7173b761479faa855f57046efc34100b28a
bamgee <petitjb05@gamil.com>
2023-05-14 20:10:09 +0900

    EDIT: layout, id 정리

--- app/src/main/java/com/example/farmmate1/TestResultActivity.kt
+++ app/src/main/java/com/example/farmmate1/TestResultActivity.kt
@@ -2,10 +2,17 @@ package com.example.farmmate1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.farmmate1.databinding.ActivityMainBinding
 
 class TestResultActivity : AppCompatActivity() {
+
+    private var mBinding: ActivityMainBinding? = null
+    private val binding get() = mBinding!!
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_test_result)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
