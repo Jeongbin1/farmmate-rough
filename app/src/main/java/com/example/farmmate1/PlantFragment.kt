387166a26920eb37cff4904bb412b24b645e5513
Youjeong <yj91322@gmail.com>
2023-05-16 17:03:51 +0900

    commit - layout, fragment

--- app/src/main/java/com/example/farmmate1/PlantFragment.kt
+++ app/src/main/java/com/example/farmmate1/PlantFragment.kt
@@ -1,31 +1,36 @@
 package com.example.farmmate1
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.DataBindingUtil.setContentView
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.farmmate1.databinding.FragmentPlantBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [PlantFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class PlantFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
+    private var _binding: FragmentPlantBinding? = null
+    private val binding get() = _binding!!
+
+    // 데이터 보존
+    companion object {
+        fun newInstance(count : Int): PlantFragment{
+            val args = Bundle()
+            args.putInt("number", count)
+            val fragment = PlantFragment()
+            fragment.arguments = args
+            return fragment
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
         }
     }
 
@@ -33,27 +38,43 @@ class PlantFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_plant, container, false)
+        _binding = FragmentPlantBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment PlantFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            PlantFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // 식물 추가 버튼 클릭 후 식물 추가 페이지로 이동
+        binding.plantAddBtn.setOnClickListener{
+            moveToAddPlantFragment()
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+
+        // 메모리 누수를 방지하기 위해 Fragment View에 대한 참조를 제거하여 가비지 컬렉터가 수거
+        _binding = null
+    }
+    private fun moveToAddPlantFragment() {
+        val transaction = parentFragmentManager
+            .beginTransaction()
+            .replace(R.id.main_fl, PlantAddFragment())
+        transaction.commit()
+    }
+
+
+    class MainViewModel : ViewModel() {
+
+        private val _count = MutableLiveData<Int>()
+        val count : LiveData<Int> get() = _count
+        init {
+            _count.value = 5
+        }
+        fun getUpdatedCount(plusCount: Int){
+            _count.value = (_count.value)?.plus(plusCount)
+        }
     }
 }
